925206984fb70e18b7deeb2e023530c4d0d0df76
Matias <mendoza.e.matias@gmail.com>
2025-09-25 19:24:43 -0300

    add new route

--- src/routes/affiliates.route.ts
+++ src/routes/affiliates.route.ts
@@ -5,4 +5,9 @@ router.get('/', (_req, res) => {
   res.status(200).json({ message: 'List of affiliates' });
 });
 
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  res.status(200).json({ message: `Details of affiliate with ID: ${id}` });
+});
+
 export default router;
