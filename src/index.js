9cf08ee02a1a75c7579d50a8252e567f85e4525c
jmogno <JoaquinMognoUnahur>
2025-10-21 15:18:46 -0300

    Agregados los endpoints Obtener todas las situaciones y Obtener una situación específica

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const affiliateRoute = require("./interface/routes/affiliateRoute");
+const therapeuticSituationRoute = require("./interface/routes/TherapeuticSituationRoute");
 const cors = require('cors');
 
 const app = express();
@@ -8,5 +9,6 @@ app.use(cors())
 app.use(bodyParser.json());
 
 app.use("/api", affiliateRoute);
+app.use("/api", therapeuticSituationRoute);
 
 app.listen(3000, () => console.log("Server running on port 3000"));
